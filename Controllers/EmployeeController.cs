de0e121affcf3128758d9ae1310c2314e2024714
raghavendra145 <109669391+raghavendra145@users.noreply.github.com>
2022-07-29 17:56:09 +0530

    push

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject_Raghavendra.Models; 
+
+
+namespace MVCProject_Raghavendra.Controllers
+{
+
+    public class EmployeeController : Controller
+    {
+        //  GET: Employee
+        public ActionResult EmployeeInfo()
+        //public string Employee = " ";
+            
+            
+        {
+
+            Employee employee = new Employee ()
+            {
+                EmployeeId = 217958,
+                EmployeeName = "Raghavendra",
+                EmployeeLocation = "Bangalore"
+            };
+
+            return View(employee);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject_Raghavendra.Models
+{
+    public class Employee 
+    {
+
+
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeLocation { get; set; }
+
+
+    }
+}
+
+
